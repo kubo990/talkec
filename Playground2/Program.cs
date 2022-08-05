c221fb426f0a3a7b1ea259998acf72e5d1045302
kubo990 <jakub.vojtasak@gmail.com>
2022-08-05 14:41:58 +0200

    init

---
+++ Playground2/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Playground2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int? index = null;
+
+            Console.WriteLine(1);
+
+            if (index.HasValue && index.Value == 1)
+            {
+                Console.WriteLine(index);
+
+            }
+            Console.WriteLine(3);
+
+        }
+    }
+}
